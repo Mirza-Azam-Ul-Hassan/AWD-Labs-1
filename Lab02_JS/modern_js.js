e74e22599e6f10e53093097b53472a15bdc31371
Azam Ali <mirzaazamulhassan4@gmail.com>
2026-03-11 16:21:53 +0500

    Your commit message

---
+++ Lab02_JS/modern_js.js
@@ -0,0 +1,7 @@
+/* modern JavaScript examples (ES2020+) */
+
+// Example: optional chaining and nullish coalescing
+const user = { name: 'Aza', prefs: { color: 'blue' } };
+console.log(user?.prefs?.color ?? 'default');
+
+console.log('modern_js.js created');
